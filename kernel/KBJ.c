59ecabb4180441d233423cf5bec58628799e63c8
Justin <justindude057@gmail.com>
2026-05-27 14:23:29 -0400

    Update KBJ.c

--- kernel/KBJ.c
+++ kernel/KBJ.c
@@ -1,3 +1,4 @@
-//this lets the bootloader give the kernel acess, KBJ is an abbrevation of KERNEL BOOTLOADER JMP.
+//this lets the bootloader give the kernel acess,
+//KBJ is an abbrevation of KERNEL BOOTLOADER JMP.
 int main()
-{ HWASG:}
+{Hwasg:}

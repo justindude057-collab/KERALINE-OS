29d2c7657fffde6031055a0e839622b5580f5095
Justin <justindude057@gmail.com>
2026-05-28 13:56:28 -0400

    Update Init.c

--- Init/Init.c
+++ Init/Init.c
@@ -1,6 +1,3 @@
-#include "Kernel/Krnlcall.h"
-#include "Kernel/Memcall.h"
-pidcall(); // assign pid
-memcall(); // declare memory allcation.
-int pointer1 = 1;
-int pointer2 = 100;
+#include "/Kernel/Memory/Krnlcall.h"
+#include "/Kernel/Memory/Memcall.h"
+int main(){}

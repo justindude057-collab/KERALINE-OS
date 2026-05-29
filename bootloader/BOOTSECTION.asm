ed7f3fae57eb8cba47fee3f8fa7b0cfcb7d538cc
Justin <justindude057@gmail.com>
2026-05-28 23:04:04 -0400

    Update BOOTSECTION.asm

--- bootloader/BOOTSECTION.asm
+++ bootloader/BOOTSECTION.asm
@@ -125,7 +125,41 @@ int 0x10
     int 0x10        
     jmp .driveC_loop 
 
+int 0x0e 
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+;e
+mov ah, 0x0E
+mov Al, '#'
+int 0x10
+mov Al, ':'
+int 0x10
 
+start:
+mov ah, 0
+Int 0x16
+mov ah, 0x0E
+int 0x10
+
+mov ah, 0
+Int 16h
+cmp ah, 1Ch
+Jz NOENTER
+
+int 0x0e 
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
+int 0x10
+
+mov Al, '#'
+int 0x10
+mov Al, ':'
+int 0x10
+NOENTER:
+;e
 halt:
 hlt
 
